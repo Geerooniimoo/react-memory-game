46254a696603ecf6688aee42b7b2f008b4965922
Geronimo Perez <Geronimo.Perez4@gmail.com>
2019-08-03 16:30:25 -0400

    Updates

---
+++ precache-manifest.d519e85490d7b03eadd81a97fbb07ad2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e045a1385d4c995d17285040bc3fa96",
+    "url": "/heroesmemorygame/index.html"
+  },
+  {
+    "revision": "31e8a6c3f3e63883c124",
+    "url": "/heroesmemorygame/static/css/main.8f3250f5.chunk.css"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/heroesmemorygame/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "31e8a6c3f3e63883c124",
+    "url": "/heroesmemorygame/static/js/main.00b652ae.chunk.js"
+  },
+  {
+    "revision": "75eb3a6860c55741b994",
+    "url": "/heroesmemorygame/static/js/runtime~main.a7092dbf.js"
+  }
+]);
